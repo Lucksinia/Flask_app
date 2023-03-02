bd6bf3211fc338843b33ae3df0ab08530c30e9a1
Lucksinia <denni334334@gmail.com>
2023-03-02 23:17:40 +0300

    started to add Boostrap... remade on Blueprints

--- blog/user/views.py
+++ blog/user/views.py
@@ -1,13 +1,26 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
+from werkzeug.exceptions import NotFound
 
 user = Blueprint("user", __name__, static_folder="../static", url_prefix="/users")
 
+USERS = {1: "Alice", 2: "Johan", 3: "Mikail"}
 
-@user.route("/")
+
+@user.route("/", endpoint="list")
 def user_list():
-    return "user_list_here"
+    return render_template(
+        "users/list_of_people.html",
+        users=USERS,
+    )
 
 
-@user.route("/<pk>")
+@user.route("/<int:pk>", endpoint="details")
 def get_user(pk: int):
-    return pk
+    try:
+        user = USERS[pk]
+    except KeyError:
+        raise NotFound(f"User {pk} was not found! Maybe try another..?")
+    return render_template(
+        "users/details.html",
+        user_name=user,
+    )
