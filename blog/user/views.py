797e600f1f694dca5ff91d93fe648801274cf978
Lucksinia <denni334334@gmail.com>
2023-03-11 14:49:57 +0300

    rerouted whole project to be able understand it.

--- blog/user/views.py
+++ blog/user/views.py
@@ -1,26 +1,27 @@
 from flask import Blueprint, render_template
+from flask_login import login_required
 from werkzeug.exceptions import NotFound
+from blog.models import User
 
 user = Blueprint("user", __name__, static_folder="../static", url_prefix="/users")
-
-USERS = {1: "Alice", 2: "Johan", 3: "Mikail"}
 
 
 @user.route("/", endpoint="list")
 def user_list():
+    users = User.query.all()
     return render_template(
         "users/list_of_people.html",
-        users=USERS,
+        users=users,
     )
 
 
-@user.route("/<int:pk>", endpoint="details")
-def get_user(pk: int):
-    try:
-        user = USERS[pk]
-    except KeyError:
+@user.route("/<int:pk>", endpoint="profile")
+@login_required
+def profile(pk: int):
+    selected_user = User.query.filter_by(id=pk).one_or_none()
+    if not selected_user:
         raise NotFound(f"User {pk} was not found! Maybe try another..?")
     return render_template(
         "users/details.html",
-        user_name=user,
+        user_name=selected_user,
     )
